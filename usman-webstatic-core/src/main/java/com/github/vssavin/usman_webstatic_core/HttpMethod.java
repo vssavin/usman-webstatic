7d32fe6b36b0d95f6b66de90028f7141ca2ef13d
vssavin <savin1801@gmail.com>
2023-12-18 14:45:21 +0300

    webstatic-core: HttpMethod - added javadoc description

--- usman-webstatic-core/src/main/java/com/github/vssavin/usman_webstatic_core/HttpMethod.java
+++ usman-webstatic-core/src/main/java/com/github/vssavin/usman_webstatic_core/HttpMethod.java
@@ -1,5 +1,10 @@
 package com.github.vssavin.usman_webstatic_core;
 
+/**
+ * Enumeration of HTTP request methods.
+ *
+ * @author vssavin on 16.12.2023.
+ */
 public enum HttpMethod {
 
     GET, HEAD, POST, PUT, PATCH, DELETE, OPTIONS, TRACE
