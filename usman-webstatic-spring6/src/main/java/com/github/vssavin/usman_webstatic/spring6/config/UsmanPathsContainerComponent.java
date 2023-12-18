9ff2c2f3ed91301562e72e8bcd7ac2d4948c317c
vssavin <savin1801@gmail.com>
2023-12-18 11:48:45 +0300

    spring5/6: added PermissionPathsContainer implementation as spring component

---
+++ usman-webstatic-spring6/src/main/java/com/github/vssavin/usman_webstatic/spring6/config/UsmanPathsContainerComponent.java
@@ -0,0 +1,22 @@
+package com.github.vssavin.usman_webstatic.spring6.config;
+
+import com.github.vssavin.usman_webstatic_core.UsmanPathsContainer;
+import com.github.vssavin.usmancore.config.UsmanUrlsConfigurer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ * A {@link com.github.vssavin.usmancore.config.PermissionPathsContainer} implementation
+ * as spring component with webstatic permission paths.
+ *
+ * @author vssavin on 18.12.2023
+ */
+@Component
+public class UsmanPathsContainerComponent extends UsmanPathsContainer {
+
+    @Autowired
+    public UsmanPathsContainerComponent(UsmanUrlsConfigurer usmanUrlsConfigurer) {
+        super(usmanUrlsConfigurer);
+    }
+
+}
