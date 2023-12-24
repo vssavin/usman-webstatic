aa2ada85f43ae80083967e391e2ce2d572f94df4
svs-devel <example@example.com>
2023-12-24 17:51:22 +0300

    spring5/6 tests: added mocked EmailService implementation

---
+++ usman-webstatic-spring5/src/test/java/com/github/vssavin/usman_webstatic/spring5/email/MockedEmailService.java
@@ -0,0 +1,50 @@
+package com.github.vssavin.usman_webstatic.spring5.email;
+
+import com.github.vssavin.usmancore.email.EmailService;
+
+import java.util.List;
+
+/**
+ * @author vssavin on 24.12.2023.
+ */
+public interface MockedEmailService extends EmailService {
+
+    List<EmailMessage> getEmailMessages();
+
+    EmailMessage getLastEmailMessage();
+
+    class EmailMessage {
+
+        private final String destination;
+
+        private final String subject;
+
+        private final String text;
+
+        public EmailMessage(String destination, String subject, String text) {
+            this.destination = destination;
+            this.subject = subject;
+            this.text = text;
+        }
+
+        public String getDestination() {
+            return destination;
+        }
+
+        public String getSubject() {
+            return subject;
+        }
+
+        public String getText() {
+            return text;
+        }
+
+        @Override
+        public String toString() {
+            return "EmailMessage{" + "destination='" + destination + '\'' + ", subject='" + subject + '\'' + ", text='"
+                    + text + '\'' + '}';
+        }
+
+    }
+
+}
---
+++ usman-webstatic-spring6/src/test/java/com/github/vssavin/usman_webstatic/spring6/email/MockedEmailServiceImpl.java
@@ -0,0 +1,34 @@
+package com.github.vssavin.usman_webstatic.spring6.email;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author vssavin on 24.12.2023.
+ */
+@Service
+@Primary
+class MockedEmailServiceImpl implements MockedEmailService {
+
+    private final List<EmailMessage> messages = new ArrayList<>();
+
+    @Override
+    public void sendSimpleMessage(String destinationEmail, String subject, String text) {
+        messages.add(new EmailMessage(destinationEmail, subject, text));
+    }
+
+    public List<EmailMessage> getEmailMessages() {
+        return messages;
+    }
+
+    public EmailMessage getLastEmailMessage() {
+        if (messages.size() == 0) {
+            throw new IllegalStateException("Emails list is empty!");
+        }
+        return messages.get(messages.size() - 1);
+    }
+
+}
