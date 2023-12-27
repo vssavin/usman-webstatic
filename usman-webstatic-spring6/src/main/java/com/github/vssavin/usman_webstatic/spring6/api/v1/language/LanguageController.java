0cd4fc17e830728a0c415dd5fe3f33534ffb6788
vssavin <savin1801@gmail.com>
2023-12-27 14:19:35 +0300

    spring5/6: LanguageController - added getDefaultLanguage mapping

--- usman-webstatic-spring6/src/main/java/com/github/vssavin/usman_webstatic/spring6/api/v1/language/LanguageController.java
+++ usman-webstatic-spring6/src/main/java/com/github/vssavin/usman_webstatic/spring6/api/v1/language/LanguageController.java
@@ -1,7 +1,9 @@
 package com.github.vssavin.usman_webstatic.spring6.api.v1.language;
 
 import com.github.vssavin.usman_webstatic_core.UsmanLocaleConfig;
+import com.github.vssavin.usman_webstatic_core.UsmanWebstaticConfigurer;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,9 +18,29 @@ import java.util.Map;
 @RestController
 class LanguageController {
 
-    @GetMapping("/usman/v1/languages")
+    private final UsmanWebstaticConfigurer webstaticConfigurer;
+
+    LanguageController(UsmanWebstaticConfigurer webstaticConfigurer) {
+        this.webstaticConfigurer = webstaticConfigurer;
+    }
+
+    @GetMapping(value = "/usman/v1/languages", produces = MediaType.APPLICATION_JSON_VALUE)
     ResponseEntity<Map<String, String>> getLanguage() {
         return new ResponseEntity<>(UsmanLocaleConfig.getAvailableLanguages(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/usman/v1/languages/default", produces = MediaType.APPLICATION_JSON_VALUE)
+    ResponseEntity<String> getDefaultLanguage() {
+        String defaultLanguage = UsmanLocaleConfig.getAvailableLanguages()
+            .get(webstaticConfigurer.getDefaultLanguage());
+        if (defaultLanguage == null) {
+            defaultLanguage = "";
+        }
+        else {
+            defaultLanguage = webstaticConfigurer.getDefaultLanguage();
+        }
+
+        return new ResponseEntity<>("\"" + defaultLanguage + "\"", HttpStatus.OK);
     }
 
 }
